8d8c97402b197d067f8f6123f15e08dd998f4537
DevSankhadeep <ssaankhadeepdas@gmail.com>
2025-11-22 16:33:41 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -13,13 +13,13 @@ app.use(cors({
   origin: process.env.VITE_BASEURL   // replace '*' with specific URL in production
 }));
 
-const MONGODB_URI = process.env.DB_URL;
-if (!MONGODB_URI) {
+const MONGO_URI = process.env.DB_URL;
+if (!MONGO_URI) {
   console.error('MONGODB_URI not set');
   process.exit(1);
 }
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
