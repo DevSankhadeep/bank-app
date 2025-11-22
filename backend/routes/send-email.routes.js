0a5de2dc468459c3e26e2652d9fad6ca2b8d61f5
DevSankhadeep <ssaankhadeepdas@gmail.com>
2025-11-22 13:23:59 +0530

    update

--- backend/routes/send-email.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const emailController = require("../controller/email.controller");
-
-router.post("/", emailController.sendEmail);
-module.exports = router;
